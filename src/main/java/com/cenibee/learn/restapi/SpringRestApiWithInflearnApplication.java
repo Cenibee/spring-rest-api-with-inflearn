6261394e4eb6836bde174b964de6645d2e916a63
cenibee <ksj3452@naver.com>
2021-02-21 19:15:58 +0900

    :tada: Begin a Project

---
+++ src/main/java/com/cenibee/learn/restapi/SpringRestApiWithInflearnApplication.java
@@ -0,0 +1,13 @@
+package com.cenibee.learn.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiWithInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiWithInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cenibee/learn/restapi/SpringRestApiWithInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenibee.learn.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiWithInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
