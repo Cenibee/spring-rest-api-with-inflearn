05109fdcc5395831ee6a8192b7a62c92895e978a
cenibee <ksj3452@naver.com>
2021-02-21 21:42:01 +0900

    :sparkles: Create operation for Event with Test

--- src/test/java/com/cenibee/learn/restapi/events/EventControllerTests.java
+++ src/test/java/com/cenibee/learn/restapi/events/EventControllerTests.java
@@ -1,9 +1,19 @@
 package com.cenibee.learn.restapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class EventControllerTests {
@@ -11,13 +21,30 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void createEvent() throws Exception {
-        // TODO 
-//        mockMvc.perform(post("/api/events")
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .accept(MediaTypes.HAL_JSON))
-//                .andExpect(status().isCreated());
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 2, 22, 21, 11))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 2, 23, 21, 11))
+                .beginEventDateTime(LocalDateTime.of(2021, 2, 23, 21, 11))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 
 }
