38c69e3c52337d165ef6d581348eb506d2cfdfd9
cenibee <ksj3452@naver.com>
2021-02-22 01:12:38 +0900

    :wrench: Responding bad_request when invalid attributes are requested

--- src/test/java/com/cenibee/learn/restapi/events/EventControllerTests.java
+++ src/test/java/com/cenibee/learn/restapi/events/EventControllerTests.java
@@ -29,6 +29,34 @@ public class EventControllerTests {
 
     @Test
     void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 2, 22, 21, 11))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 2, 23, 21, 11))
+                .beginEventDateTime(LocalDateTime.of(2021, 2, 23, 21, 11))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.PUBLISHED)));
+    }
+
+    @Test
+    void createEvent_Bad() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -46,17 +74,11 @@ public class EventControllerTests {
                 .build();
 
         mockMvc.perform(post("/api/events")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON)
-                    .content(objectMapper.writeValueAsString(event)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.PUBLISHED)));
+                .andExpect(status().isBadRequest());
     }
 
 }
