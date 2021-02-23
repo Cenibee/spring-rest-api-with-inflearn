b9ef6f8a5f77dc376def333cc5b82772a9f9b482
cenibee <ksj3452@naver.com>
2021-02-23 23:58:16 +0900

    :ambulance: Adding _links in responses with test

--- src/test/java/com/cenibee/learn/restapi/events/EventControllerTests.java
+++ src/test/java/com/cenibee/learn/restapi/events/EventControllerTests.java
@@ -56,7 +56,10 @@ public class EventControllerTests {
                 .andExpect(jsonPath("id").value(Matchers.not(100)))
                 .andExpect(jsonPath("free").value(false))
                 .andExpect(jsonPath("offline").value(true))
-                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.PUBLISHED)));
+                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.PUBLISHED)))
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.query-events").exists())
+                .andExpect(jsonPath("_links.update-event").exists());
     }
 
     @Test
