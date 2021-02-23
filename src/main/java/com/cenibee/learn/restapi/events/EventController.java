6bdeba513d1171ee69957a0243e72c5ccf303358
cenibee <ksj3452@naver.com>
2021-02-23 23:27:21 +0900

    :label: Adding _links in responses with test

--- src/main/java/com/cenibee/learn/restapi/events/EventController.java
+++ src/main/java/com/cenibee/learn/restapi/events/EventController.java
@@ -3,6 +3,7 @@ package com.cenibee.learn.restapi.events;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -46,8 +47,14 @@ public class EventController {
         Event event = modelMapper.map(eventDto, Event.class);
         event.update();
         Event newEvent = this.eventRepository.save(event);
-        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createdUri).body(event);
+
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdUri = selfLinkBuilder.toUri();
+        return ResponseEntity.created(createdUri).body(EventResource.of(event)
+                .add(selfLinkBuilder.withSelfRel())
+                .add(selfLinkBuilder.withRel("update-event"))
+                .add(linkTo(EventController.class).withRel("query-events"))
+        );
     }
 
 }
---
+++ src/main/java/com/cenibee/learn/restapi/events/EventResource.java
@@ -0,0 +1,6 @@
+package com.cenibee.learn.restapi.events;
+
+import org.springframework.hateoas.EntityModel;
+
+public class EventResource extends EntityModel<Event> {
+}
