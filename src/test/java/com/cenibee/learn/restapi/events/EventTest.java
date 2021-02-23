19e3324ccf809486928af5166f9eafd320ed2800
cenibee <ksj3452@naver.com>
2021-02-23 23:25:51 +0900

    :label: Adding Event Validation with Parameterized Test

--- src/test/java/com/cenibee/learn/restapi/events/EventTest.java
+++ src/test/java/com/cenibee/learn/restapi/events/EventTest.java
@@ -1,8 +1,14 @@
 package com.cenibee.learn.restapi.events;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class EventTest {
     @Test
@@ -28,5 +34,54 @@ class EventTest {
         // Then
         assertThat(event.getName()).isEqualTo(name);
         assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testFree(int basePrice, int maxPrice, boolean isFree) {
+        // Given:
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        // When:
+        event.update();
+
+        // Then:
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+
+    static Stream<Arguments> testFree() {
+        return Stream.of(
+                arguments(0, 0, true),
+                arguments(100, 0, false),
+                arguments(0, 100, false),
+                arguments(100, 200, false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testOffline(String location, boolean isOffline) {
+        // Given:
+        Event event = Event.builder()
+                .location(location)
+                .build();
+
+        // When:
+        event.update();
+
+        // Then:
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
+
+    static Stream<Arguments> testOffline() {
+        return Stream.of(
+                arguments("강남", true),
+                arguments("", false),
+                arguments("   ", false),
+                arguments(null, false)
+        );
     }
 }
