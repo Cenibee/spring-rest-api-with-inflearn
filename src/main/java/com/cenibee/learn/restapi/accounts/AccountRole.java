cf34763e2ec479b2a96aaa9087b8bd892d5cc8eb
cenibee <ksj3452@naver.com>
2021-03-03 22:52:45 +0900

    :label: Add Account POJO

---
+++ src/main/java/com/cenibee/learn/restapi/accounts/AccountRole.java
@@ -0,0 +1,5 @@
+package com.cenibee.learn.restapi.accounts;
+
+public enum AccountRole {
+    ADMIN, USER
+}
--- src/main/java/com/cenibee/learn/restapi/events/Event.java
+++ src/main/java/com/cenibee/learn/restapi/events/Event.java
@@ -1,5 +1,6 @@
 package com.cenibee.learn.restapi.events;
 
+import com.cenibee.learn.restapi.accounts.Account;
 import lombok.*;
 
 import javax.persistence.*;
@@ -30,18 +31,11 @@ public class Event {
     private boolean free;
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
+    @ManyToOne
+    private Account manager;
 
     public void update() {
-        if (this.basePrice == 0 && this.maxPrice == 0) {
-            this.free = true;
-        } else {
-            this.free = false;
-        }
-
-        if (this.location == null || this.location.isBlank()) {
-            this.offline = false;
-        } else {
-            this.offline = true;
-        }
+        this.free = this.basePrice == 0 && this.maxPrice == 0;
+        this.offline = this.location != null && !this.location.isBlank();
     }
 }
