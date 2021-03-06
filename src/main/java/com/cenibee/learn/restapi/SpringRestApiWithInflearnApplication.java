d286213596e62209c7bc9e63441d9e3c1e6e5685
cenibee <ksj3452@naver.com>
2021-03-06 15:08:09 +0900

    :passport_control: Apply Authentication except docs and resources

--- src/main/java/com/cenibee/learn/restapi/SpringRestApiWithInflearnApplication.java
+++ src/main/java/com/cenibee/learn/restapi/SpringRestApiWithInflearnApplication.java
@@ -12,8 +12,4 @@ public class SpringRestApiWithInflearnApplication {
         SpringApplication.run(SpringRestApiWithInflearnApplication.class, args);
     }
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 }
