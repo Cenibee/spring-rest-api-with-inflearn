1124b8d5e789bdfd5075c6ca3e17c5bf334efe2f
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-03-09 18:35:17 +0900

    :memo: Document index

--- src/test/java/com/cenibee/learn/restapi/index/IndexControllerTest.java
+++ src/test/java/com/cenibee/learn/restapi/index/IndexControllerTest.java
@@ -3,8 +3,15 @@ package com.cenibee.learn.restapi.index;
 import com.cenibee.learn.restapi.common.BaseControllerTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.hypermedia.HypermediaDocumentation;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,7 +22,15 @@ public class IndexControllerTest extends BaseControllerTest {
     public void index() throws Exception {
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("_links.events").exists());
+                .andExpect(jsonPath("_links.events").exists())
+                .andDo(document("index",
+                        links(
+                                linkWithRel("events").description("link to query events")
+                        ),
+                        responseFields(
+                                subsectionWithPath("_links").description("relational links")
+                        )
+                ));
     }
 
 }
